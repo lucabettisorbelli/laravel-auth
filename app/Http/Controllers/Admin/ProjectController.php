346709e1273845f97435b528c4956d4348c97311
luca betti sorbelli <luca betti sorbelli>
2023-07-11 14:58:09 +0200

    fix

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -16,7 +16,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $projects = Project::all();
+        return view('admin.project.index', compact('projects'));
     }
 
     /**
